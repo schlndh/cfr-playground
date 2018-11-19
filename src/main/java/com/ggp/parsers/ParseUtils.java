9226aa5c2ec187b5c0874fdb43cff3d22ccb4bf4
Hynek Schlindenbuch <schlindenbuch.h@seznam.cz>
2018-11-19 10:36:38 +0100

    remove previous game-specific parser

--- src/main/java/com/ggp/parsers/ParseUtils.java
+++ src/main/java/com/ggp/parsers/ParseUtils.java
@@ -1,60 +1,10 @@
 package com.ggp.parsers;
 
-import com.ggp.IGameDescription;
 import com.ggp.parsers.visitors.ConfigKeyVisitor;
-import com.ggp.utils.GameRepository;
-import com.ggp.utils.recall.PerfectRecallGameDescriptionWrapper;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.misc.ParseCancellationException;
-import org.antlr.v4.runtime.tree.TerminalNode;
 
 public class ParseUtils {
-    public static IGameDescription parseGameDescription(String str) {
-        CharStream inputStream = CharStreams.fromString(str);
-        GamesLexer lexer = new GamesLexer(inputStream);
-        CommonTokenStream tokenStream = new CommonTokenStream(lexer);
-        GamesParser gp = new GamesParser(tokenStream);
-        gp.setErrorHandler(new BailErrorStrategy());
-
-        GamesVisitor<IGameDescription> gamesVisitor = new GamesBaseVisitor<IGameDescription>() {
-            @Override
-            public IGameDescription visitLeducPoker(GamesParser.LeducPokerContext ctx) {
-                return GameRepository.leducPoker(parseInt(ctx.POSINT(0)), parseInt(ctx.POSINT(1)));
-            }
-
-            @Override
-            public IGameDescription visitIiGoofspiel(GamesParser.IiGoofspielContext ctx) {
-                return GameRepository.iiGoofspiel(parseInt(ctx.POSINT()));
-            }
-
-            @Override
-            public IGameDescription visitRps(GamesParser.RpsContext ctx) {
-                return GameRepository.rps(parseInt(ctx.POSINT()));
-            }
-
-            @Override
-            public IGameDescription visitKriegTTT(GamesParser.KriegTTTContext ctx) {
-                return GameRepository.kriegTTT();
-            }
-
-            @Override
-            public IGameDescription visitPerfRecall(GamesParser.PerfRecallContext ctx) {
-                return new PerfectRecallGameDescriptionWrapper(ctx.game().accept(this));
-            }
-
-            private int parseInt(TerminalNode node) {
-                return Integer.valueOf(node.getSymbol().getText());
-            }
-        };
-
-        try {
-            GamesParser.GameContext gameCtx = gp.game();
-            return gamesVisitor.visit(gameCtx);
-        } catch (ParseCancellationException e) {
-        }
-        return null;
-    }
-
     public static ConfigKey parseConfigKey(String str) {
         CharStream inputStream = CharStreams.fromString(str);
         ConfigKeyLexer lexer = new ConfigKeyLexer(inputStream);
--- src/test/java/com/ggp/parsers/ParseUtilsTest.java
+++ src/test/java/com/ggp/parsers/ParseUtilsTest.java
@@ -1,48 +1,12 @@
 package com.ggp.parsers;
 
-import com.ggp.IGameDescription;
-import com.ggp.utils.GameRepository;
-import com.ggp.utils.recall.PerfectRecallGameDescriptionWrapper;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParseUtilsTest {
-
-    @ParameterizedTest(name = "{1}")
-    @MethodSource("correctGameDescriptions")
-    void testParseGameDescription_correct(IGameDescription expected, String str) {
-        assertEquals(expected, ParseUtils.parseGameDescription(str));
-    }
-
-    static Stream<Arguments> correctGameDescriptions() {
-        return Stream.of(
-                Arguments.of(GameRepository.leducPoker(5, 9), "LeducPoker{5,9}"),
-                Arguments.of(GameRepository.leducPoker(9, 5), "LeducPoker{9,5}"),
-                Arguments.of(GameRepository.iiGoofspiel(5), "IIGoofspiel{5}"),
-                Arguments.of(GameRepository.rps(11), "RockPaperScissors{11}"),
-                Arguments.of(GameRepository.kriegTTT(), "KriegTicTacToe"),
-                Arguments.of(new PerfectRecallGameDescriptionWrapper(GameRepository.leducPoker(5, 9)), "PerfectRecall{LeducPoker{5,9}}")
-        );
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"", "aaaa", "Leduc Poker{5,9}", "LeducPoker", "LeducPoker{5}", "LeducPoker{,}", "LeducPoker{-1, -1}"})
-    void testParseGameDescription_incorrect(String str) {
-        assertNull(ParseUtils.parseGameDescription(str));
-    }
-
-    @Test
-    void testParseGameDescription_whitespaces() {
-        assertEquals(GameRepository.leducPoker(5, 9), ParseUtils.parseGameDescription("LeducPoker \t\n{5\n\n , 9 } \t\n\r"));
-    }
 
     @Test
     void testParseConfigKey() throws Exception {
