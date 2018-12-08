a75f55c0abbdd01c2bb1494120a1ed50b322cb31
Hynek Schlindenbuch <schlindenbuch.h@seznam.cz>
2018-12-08 14:13:11 +0100

    clarify IInformationSet's definition

--- src/main/java/com/ggp/IInformationSet.java
+++ src/main/java/com/ggp/IInformationSet.java
@@ -4,11 +4,23 @@ import java.io.Serializable;
 import java.util.List;
 
 /**
+ * Information set interface.
+ *
+ * IS I is in acting state when obtained from CIS by calling getInfoSetForActingPlayer or there exists
+ * a path from CIS s (initial state or owner is the acting player) to CIS t (owner is the acting player) such that
+ * starting with J = s.getInfoSetForPlayer(owner) and sequentially updating J with owner's actions and percepts on path to t
+ * leads to I.
+ *
+ * Corresponds to augmented information set when in non-acting state.
+ * Methods dealing with actions have undefined  behavior when in non-acting state.
+ *
  * Implementing class must be immutable and must suitably override hashCode and equals methods.
  */
 public interface IInformationSet extends Serializable {
     /**
      * Generate information set after taking an action.
+     *
+     * Undefined behavior in non-acting state.
      * @param a action
      * @return new IS or null if action is not legal.
      */
@@ -23,12 +35,16 @@ public interface IInformationSet extends Serializable {
 
     /**
      * Get list of legal actions.
+     *
+     * Undefined behavior in non-acting state.
      * @return list or null if there are no legal actions (IS is terminal).
      */
     List<IAction> getLegalActions();
 
     /**
      * Check whether given action is legal in this IS.
+     *
+     * Undefined behavior in non-acting state.
      * @param a
      * @return
      */
