9f6aa363c17f3d5d02ccc7d3e668a4df0e15b2b0
Hynek Schlindenbuch <schlindenbuch.h@seznam.cz>
2019-01-04 17:11:45 +0100

    precompute hash in PerceptListNode

--- src/main/java/com/ggp/utils/recall/PerceptListNode.java
+++ src/main/java/com/ggp/utils/recall/PerceptListNode.java
@@ -9,23 +9,26 @@ class PerceptListNode implements Serializable {
     private static final long serialVersionUID = 1L;
     public final PerceptListNode previous;
     public final IPercept percept;
+    private final int hash;
 
     public PerceptListNode(PerceptListNode previous, IPercept percept) {
         this.previous = previous;
         this.percept = percept;
+        this.hash = Objects.hash(previous, percept);
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
+        if (this.hashCode() != o.hashCode()) return false;
         PerceptListNode that = (PerceptListNode) o;
         return Objects.equals(percept, that.percept) &&Objects.equals(previous, that.previous);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(previous, percept);
+        return hash;
     }
 
     public PerceptListNode getPrevious() {
