632cd47bf62c888be1f20cd2f516d811d182a7ba
Hynek Schlindenbuch <schlindenbuch.h@seznam.cz>
2019-06-26 11:06:35 +0200

    remove unused code

--- src/main/java/com/ggp/cli/CliHelper.java
+++
@@ -1,8 +0,0 @@
-package com.ggp.cli;
-
-public class CliHelper {
-    public static String[] splitArgString(String args) {
-        if (args == null || args.isEmpty()) return new String[]{};
-        return args.split(" ");
-    }
-}
